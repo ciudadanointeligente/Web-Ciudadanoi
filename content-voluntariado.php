9c1edc31477cf7beed70fff950faaa3fedbf5d0e
Kathe Malis <kathemalis@gmail.com>
2012-08-09 17:51:14 -0400

    Cambio de disposición contenido del Post-it
    Elimina desde y persona de contacto
    Cambia  distribución de la info desde punteo a tipo relato

--- content-voluntariado.php
+++ content-voluntariado.php
@@ -15,23 +15,25 @@
                 
                 
                  
-                 <div class="areavoluntariado"><?php echo(types_render_field("area", array('show_name' => 'false'))); ?>
-                 </div>
+                 <!--<div class="areavoluntariado"><?php echo(types_render_field("area", array('show_name' => 'false'))); ?>
+                 </div>-->
                  <hr>
-                 
-              
-                 <div class="fecha"> Desde: <?php echo(types_render_field("desde", array('show_name' => 'false'))); ?> </div>
-             	 
-                 
-                 <div class="fecha"> Hasta: <?php echo(types_render_field("termino", array('show_name' => 'false'))); ?> </div>
-             	 <hr>
              	
-                <div class="email-contacto">Interesados contactarse a: <?php echo(types_render_field("email-contacto", array('show_name' => 'false'))); ?> </div> </a> 
+               <!-- <div class="email-contacto">Interesados contactarse a: <?php echo(types_render_field("email-contacto", array('show_name' => 'false'))); ?> 
+                <hr> </div> </a> -->
+                
+                <!--<div class="fecha"> Desde: <?php echo(types_render_field("desde", array('show_name' => 'false'))); ?> </div> -->
+             	     <div class="numerovol">Necesitamos
+                  <span class="numerovol"> <?php echo(types_render_field("numero-vol", array('show_name' => 'false'))); ?></span> 	voluntarios <br /> para trabajar con nuestro equipo de <?php echo(types_render_field("proyecto-area", array('show_name' => 'false'))); ?> </div>
+               
                 <hr>
                  
-                Cantidad de voluntarios:
-                  <div class="numerovol">  <?php echo(types_render_field("numero-vol", array('show_name' => 'false'))); ?>
-                </div> 
+                 <div class="fecha"> Inscríbete hasta el <?php echo(types_render_field("termino", array('show_name' => 'false'))); ?> </div>
+             	 <hr>
+                 
+                
+            
+                <div class="verMas"> <a href= <?php the_permalink(); ?> >Ver +</a></div>
                  </header>
                 
                  
